4ce5e71db9b433fa13b9ebd33858eaa8a9f6b05f
Alwiii <210209501011@student.unm.ac.id>
2024-12-19 20:36:13 +0800

    menambahkan inputName

--- script/app.js
+++ script/app.js
@@ -3,18 +3,41 @@ const app = Vue.createApp({
     return {
       state: true,
       inputName: "",
-      name: [],
+      names: [],
+      error: "",
+      showError: false,
     };
+  },
+  computed: {
+    isReady() {
+      return this.names.length > 1;
+    },
   },
   methods: {
     addNameToList() {
       const userName = this.inputName;
       if (this.validate(userName)) {
-        this.name.push(userName);
+        this.names.push(userName);
         this.inputName = "";
+        this.showError = false;
       } else {
-        console.log("Invalid name");
+        this.showError = true;
       }
+    },
+    validate(value) {
+      this.error = "";
+      if (value === "") {
+        this.error = "Name cannot be empty";
+        return false;
+      }
+      if (this.names.includes(value)) {
+        this.error = "Name already exists";
+        return false;
+      }
+      return true;
+    },
+    removeName(index) {
+      this.names.splice(index, 1);
     },
   },
 }).mount("#app");
